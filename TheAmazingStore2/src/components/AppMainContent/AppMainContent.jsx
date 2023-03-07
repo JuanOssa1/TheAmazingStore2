5fd3be77203d9e5d08065e41f31843bdde59fe31
Juan Ossa Ossa <juan.ossao@perficient.com>
2023-03-07 17:47:49 -0500

    Maquetado card agregado

--- TheAmazingStore2/src/components/AppMainContent/AppMainContent.jsx
+++ TheAmazingStore2/src/components/AppMainContent/AppMainContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import BigInput from '../UI/BigInput/BigInput';
 import ButtonBlue from '../UI/ButtonBlue/ButtonBlue';
+import HotelCard from '../UI/HotelCard/HotelCard';
 import styles from './AppMainContent.module.scss'
 
 export default function AppMainContent() {
@@ -10,6 +11,7 @@ export default function AppMainContent() {
         <BigInput title="Correo electrónico: "></BigInput>
         <BigInput title="Contraseña: " type="password" marginTop="15px"></BigInput>
         <ButtonBlue text="Ingresar" margin="47px 0 17px 0"></ButtonBlue>
+        
         <p className={`${styles['main-content__register']}`}>¿Aún no tenes cuenta? <a>Registrate</a></p>
     </div>
   );
--- TheAmazingStore2/src/components/UI/HotelCard/HotelCard.jsx
+++ TheAmazingStore2/src/components/UI/HotelCard/HotelCard.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import ButtonBlue from '../ButtonBlue/ButtonBlue'
+import {AiFillStar, AiOutlineWifi} from "react-icons/ai"
+import {GoLocation} from 'react-icons/go'
+import {TbSwimming} from 'react-icons/tb'
+//import styles from './HotelCard.module.scss'
+import './HotelCard.module.scss'
 
 export default function HotelCard() {
   return (
     <section className='hotel-card'>
-        <img src="" alt="" className='hotel-card__image'/>
-        <div className='hotel-card__info'>
-
-        </div>
+        <img src="https://cf.bstatic.com/xdata/images/hotel/max500/359985903.jpg?k=d279753b694d94521eefe3ae7d1869de095a1cd9dd5fd8864b15c77fcd4adb12&o=&hp=1" alt="No cargo" className='hotel-card__image'/>
+        <section className='hotel-card__info'>
+            <span className='info-general'>
+                <p className='info-general__stars'>HOTEL <AiFillStar/></p>
+                <h1 className='info-general__title'>Hermitage Hotel</h1>   
+            </span>
+            <span className='info-score'>
+                <p className='info-score__value'>8</p>
+                <p className='info-score__text'>Muy bueno</p>
+            </span>
+        </section>
+        <section className='hotel-card__specs'>
+            <span className='hotel-location'>
+                <GoLocation/>
+                <p className='hotel-location__description'>A 940 m del centro <a>MOSTRAR EN EL MAPA</a></p>
+            </span>
+            <span className='hotel-highlights'>
+                <AiOutlineWifi/>
+                <TbSwimming/>
+            </span>
+        </section>
+        <p className='hotel-card__description'>En el corazón de San Telmo, disfruta de un albergue inspirado en las pasiones de Buenos Aires.<a>más...</a></p>
+        <ButtonBlue text="ver mas" width="296px"/>
     </section>
   )
 }
